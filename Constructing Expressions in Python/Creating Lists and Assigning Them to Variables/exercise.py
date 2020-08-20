8d530b8edb2eda33ee2b8fae3b2850fdd2e6dea2
Leon Shpaner <62681831+lshpaner@users.noreply.github.com>
2020-08-20 09:04:35 -0700

    committed first exercise

---
+++ Constructing Expressions in Python/Creating Lists and Assigning Them to Variables/exercise.py
@@ -0,0 +1,19 @@
+"""
+Creating Lists and Assigning Them to Variables
+Author: Leon Shpaner
+Date:   July 12, 2020
+"""
+
+my_list = [1,2,3,4]
+print(my_list)
+
+pos2 = my_list[2]
+print(pos2)
+
+my_list[2] = 'C'
+print(my_list)
+
+# 
+
+my_second_list = [1,2.01,'123',[5,6,7]]
+print(my_second_list)
