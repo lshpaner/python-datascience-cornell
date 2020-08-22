cc8d8bddeae109633f8264b19ad209f2134fe777
Leon Shpaner <lshpaner@ucla.edu>
2020-08-22 11:22:02 -0700

    created dictionaries

---
+++ Constructing Expressions in Python/Creating Dictionaries and Assigning to Variables/exercise.py
@@ -0,0 +1,9 @@
+"""
+Creating Lists and Assigning Them to Variables in Python
+Author: Leon Shpaner
+Date:   July 16, 2020
+"""
+my_dict = {"a": 1, "b": 2, "c": 3, "d":4}
+my_dict['c'] = 30
+print(my_dict)
+val_e = my_dict['e']
