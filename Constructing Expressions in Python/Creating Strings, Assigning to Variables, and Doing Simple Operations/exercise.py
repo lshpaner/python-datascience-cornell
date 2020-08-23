007af1445268035cda3753e0c3cab207aaff030e
Leon Shpaner <lshpaner@ucla.edu>
2020-08-23 09:26:35 -0700

    updated project - strings

---
+++ Constructing Expressions in Python/Creating Strings, Assigning to Variables, and Doing Simple Operations/exercise.py
@@ -0,0 +1,14 @@
+"""
+Creating Strings, Assigning to Variables, and Doing Simple Operations
+Author: Leon Shpaner
+Date:   July 18, 2020
+"""
+s1 = 'abc'
+s2 = '123'
+s3 = s1+s2
+print(s3)
+pos0 = s1[0]
+print(pos0)
+pos2 = s3[2]
+print(pos2)
+s4 = s1+' '+s2
