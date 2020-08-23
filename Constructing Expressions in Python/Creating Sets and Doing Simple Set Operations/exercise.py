63026d9bfd71069da29cc60c83ad416f0cda12a6
Leon Shpaner <lshpaner@ucla.edu>
2020-08-23 09:21:27 -0700

    updated project - sets

---
+++ Constructing Expressions in Python/Creating Sets and Doing Simple Set Operations/exercise.py
@@ -0,0 +1,16 @@
+"""
+Creating Sets and Doing Simple Set Operations
+Author: Leon Shpaner
+Date:   July 17, 2020
+"""
+
+my_marbles = {'black', 'green', 'blue', 'red', 'purple', 'yellow', 'cyan'}
+
+your_marbles = {'green', 'magenta', 'red', 'brown', 'black', 'gray'}
+
+print (my_marbles & your_marbles)
+
+regionA = {'blue','purple','yellow', 'cyan'}
+regionB = {'red','black','green'}
+regionC = {'magenta','brown','gray'}
+regionABC = {'black', 'blue', 'red', 'purple', 'yellow', 'cyan', 'green', 'magenta', 'brown', 'gray'}
